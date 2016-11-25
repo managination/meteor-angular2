272231e56f3be8c46e16486c6ef1792e09cbeb40
Micha Roon <micha.roon@managination.com>
2016-11-25 07:38:15 +0100

    route

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -1,23 +1,9 @@
-import { Component } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-
-import { Parties } from '../../../both/collections/parties.collection';
-import { Party } from '../../../both/models/party.model';
-
-import template from './app.component.html';
+import {Component} from "@angular/core";
+import template from "./app.component.html";
 
 @Component({
   selector: 'app',
   template
 })
-export class AppComponent {
-  parties: Observable<Party[]>;
+export class AppComponent {}
 
-  constructor() {
-    this.parties = Parties.find({}).zone();
-  }
-
-  removeParty(party: Party): void {
-    Parties.remove(party._id);
-  }
-}
--- client/imports/app/app.module.ts
+++ client/imports/app/app.module.ts
@@ -1,22 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {RouterModule} from "@angular/router";
 
-import { AppComponent } from './app.component';
-import { PARTIES_DECLARATIONS } from './parties';
+import {AppComponent} from './app.component';
+import {PARTIES_DECLARATIONS} from './parties';
+import {routes} from './app.routes';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    ReactiveFormsModule
-  ],
-  declarations: [
-    AppComponent,
-    ...PARTIES_DECLARATIONS
-  ],
-  bootstrap: [
-    AppComponent
-  ]
+    imports: [
+        BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
+        RouterModule.forRoot(routes)
+    ],
+    declarations: [
+        AppComponent,
+        ...PARTIES_DECLARATIONS
+    ],
+    bootstrap: [
+        AppComponent
+    ]
 })
-export class AppModule {}
+export class AppModule {
+}
